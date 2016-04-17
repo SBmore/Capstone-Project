2ab5960dda54b2c4f5332d5078509cdd532d6f69
SBmore <s.blakemore87@gmail.com>
2016-04-17 13:01:47 +0100

    Add font resources

--- BulletPoints/app/src/main/java/app/com/example/android/bulletpoints/MainActivityFragment.java
+++ BulletPoints/app/src/main/java/app/com/example/android/bulletpoints/MainActivityFragment.java
@@ -1,7 +1,8 @@
 package app.com.example.android.bulletpoints;
 
-import android.support.v4.app.Fragment;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +12,20 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    private Typeface mRobotoReg;
+    private Typeface mRobotoThin;
+    
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        mRobotoReg = Typeface.createFromAsset(getResources().getAssets(), "Roboto-Regular.ttf");
+        mRobotoThin = Typeface.createFromAsset(getResources().getAssets(), "Roboto-Light.ttf");
+
+        return root;
     }
 }
