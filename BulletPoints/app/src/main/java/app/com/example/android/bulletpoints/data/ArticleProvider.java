ba18f2f0c8d19d2c32267b34b436c754a29e2d51
SBmore <s.blakemore87@gmail.com>
2016-04-25 07:39:36 +0100

    Refactor for scalability

--- BulletPoints/app/src/main/java/app/com/example/android/bulletpoints/data/ArticleProvider.java
+++ BulletPoints/app/src/main/java/app/com/example/android/bulletpoints/data/ArticleProvider.java
@@ -12,13 +12,26 @@ import net.simonvt.schematic.annotation.TableEndpoint;
 @ContentProvider(authority = ArticleProvider.AUTHORITY, database = ArticleDatabase.class)
 public class ArticleProvider {
     public static final String AUTHORITY = "app.com.example.android.bulletpoints.data.ArticleProvider";
+    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
 
-    @TableEndpoint(table = ArticleDatabase.ARTICLES) public static class Articles {
+    interface Path {
+        String ARTICLES = "articles";
+    }
 
+    private static Uri buildUri(String... params) {
+        Uri.Builder builder = BASE_CONTENT_URI.buildUpon();
+        for (int i = 0; i < params.length; i += 1) {
+            builder.appendPath(params[i]);
+        }
+        return builder.build();
+    }
+
+    @TableEndpoint(table = ArticleDatabase.ARTICLES)
+    public static class Articles {
         @ContentUri(
-                path = "articles",
+                path = Path.ARTICLES,
                 type = "vnd.android.cursor.dir/list",
                 defaultSort = ArticleContract.ArticleColumns.TITLE + " ASC")
-        public static final Uri ARTICLES = Uri.parse("content://" + AUTHORITY + "/articles");
+        public static final Uri CONTENT_URI = buildUri(Path.ARTICLES);
     }
 }
