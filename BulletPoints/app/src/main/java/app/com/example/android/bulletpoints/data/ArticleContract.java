93671a4d92452445bc2cd2fcb1ebe603f3d2a746
SBmore <s.blakemore87@gmail.com>
2016-04-29 22:40:17 +0100

    Add columns for main content

--- BulletPoints/app/src/main/java/app/com/example/android/bulletpoints/data/ArticleContract.java
+++ BulletPoints/app/src/main/java/app/com/example/android/bulletpoints/data/ArticleContract.java
@@ -14,8 +14,7 @@ import net.simonvt.schematic.annotation.Unique;
 public class ArticleContract {
     public interface ArticleColumns {
 
-        @DataType(DataType.Type.INTEGER) @PrimaryKey
-        @AutoIncrement
+        @DataType(DataType.Type.INTEGER) @PrimaryKey @AutoIncrement
         String _ID = "_id";
 
         @DataType(DataType.Type.TEXT) @NotNull @Unique
@@ -32,5 +31,40 @@ public class ArticleContract {
 
         @DataType(DataType.Type.TEXT) @Nullable
         String IMG_URL = "img_url";
+
+        @DataType(DataType.Type.TEXT)
+        String BODY = "body";
+
+        @DataType(DataType.Type.TEXT)
+        String BULLETPOINT_1 = "bulletpoint_1";
+
+        @DataType(DataType.Type.TEXT)
+        String BULLETPOINT_2 = "bulletpoint_2";
+
+        @DataType(DataType.Type.TEXT)
+        String BULLETPOINT_3 = "bulletpoint_3";
+
+        @DataType(DataType.Type.TEXT)
+        String BULLETPOINT_4 = "bulletpoint_4";
+
+        @DataType(DataType.Type.TEXT)
+        String BULLETPOINT_5 = "bulletpoint_5";
+
+        @DataType(DataType.Type.TEXT)
+        String PARAGRAPH_1 = "paragraph_1";
+
+        @DataType(DataType.Type.TEXT)
+        String PARAGRAPH_2 = "paragraph_2";
+
+        @DataType(DataType.Type.TEXT)
+        String PARAGRAPH_3 = "paragraph_3";
+
+        @DataType(DataType.Type.TEXT)
+        String PARAGRAPH_4 = "paragraph_4";
+
+        @DataType(DataType.Type.TEXT)
+        String PARAGRAPH_5 = "paragraph_5";
+
+
     }
 }
