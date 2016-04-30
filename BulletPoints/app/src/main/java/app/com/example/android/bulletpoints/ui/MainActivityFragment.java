0096c074a3b4ee6a94b61dfbf4dcf998f688eeb6
SBmore <s.blakemore87@gmail.com>
2016-04-30 10:05:18 +0100

    Start implementation of the cursor loader
    Made MainActivityFragment implement LoaderCallbacks<Cursor> and then
    override the loader callback methods. Implemented onCreateLoader but now
    need to create a cursor adaptor to link the data up to.

--- BulletPoints/app/src/main/java/app/com/example/android/bulletpoints/ui/MainActivityFragment.java
+++ BulletPoints/app/src/main/java/app/com/example/android/bulletpoints/ui/MainActivityFragment.java
@@ -1,11 +1,12 @@
 package app.com.example.android.bulletpoints.ui;
 
-import android.content.ContentResolver;
 import android.database.Cursor;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,18 +15,18 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 import app.com.example.android.bulletpoints.R;
+import app.com.example.android.bulletpoints.data.ArticleContract;
 import app.com.example.android.bulletpoints.data.ArticleDataFetcher;
 import app.com.example.android.bulletpoints.data.ArticleProvider;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private final static String TAG = "MAIN_ACTIVITY_FRAGMENT";
     private Typeface mRobotoReg;
     private Typeface mRobotoThin;
-
 
     public MainActivityFragment() {
     }
@@ -47,27 +48,33 @@ public class MainActivityFragment extends Fragment {
         ArticleDataFetcher articleDataFetcher = new ArticleDataFetcher(getContext());
         articleDataFetcher.execute("http://feeds.skynews.com/feeds/rss/world.xml");
 
-        // TODO: temporary to check if data is being written to db
-        ContentResolver resolver = getContext().getContentResolver();
-
-        Cursor cursor = resolver.query(
-                ArticleProvider.Articles.CONTENT_URI,
-                null, null, null, null);
-
-        try {
-            cursor.moveToFirst();
-            Log.v(TAG, "Results returned: " + cursor.getCount());
-            while (!cursor.isAfterLast()) {
-                if (!cursor.isNull(6)) {
-                    Log.v(TAG, "Body: " + cursor.getString(6));
-                }
-                cursor.moveToNext();
-            }
-            cursor.close();
-        } catch (NullPointerException e) {
-            Log.e(TAG, e.toString());
-        }
-
         return root;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String sortOrder = ArticleContract.ArticleColumns.PUB_DATE + " ASC";
+        String[] columns = {ArticleContract.ArticleColumns.TITLE,
+                ArticleContract.ArticleColumns.DESCRIPTION,
+                ArticleContract.ArticleColumns.LINK,
+                ArticleContract.ArticleColumns.IMG_URL,
+                ArticleContract.ArticleColumns.PUB_DATE};
+
+        return new CursorLoader(getActivity(),
+                ArticleProvider.Articles.CONTENT_URI,
+                columns,
+                null,
+                null,
+                sortOrder);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // TODO: Make adaptor then swap cursor with data
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // TODO: Make adaptor then swap cursor with null
     }
 }
